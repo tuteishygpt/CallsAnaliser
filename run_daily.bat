72807ae9e4d3ed1268973f40e64c24862b9036bc
tuteishygpt <you@example.com>
2026-06-23 12:45:56 +0300

    make daily runner path independent

--- run_daily.bat
+++ run_daily.bat
@@ -1,4 +1,6 @@
 @echo off
-cd /d "d:\googlePRJ\Amedis"
-call venv\Scripts\activate.bat
-python -m calls_analyser.runner >> daily_log.txt 2>&1
+pushd "%~dp0"
+"venv\Scripts\python.exe" -m calls_analyser.runner >> "daily_log.txt" 2>&1
+set "EXIT_CODE=%ERRORLEVEL%"
+popd
+exit /b %EXIT_CODE%
